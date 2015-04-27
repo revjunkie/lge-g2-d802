d2c72c2ea40006d5f8bd55cf4ab59c5c652869af
revjunkie <rajibrahim@rocketmail.com>
2015-04-27 23:41:06 +0800

    mm: Enhance per process reclaim to consider shared pages
    Some pages could be shared by several processes. (ex, libc)
    In case of that, it's too bad to reclaim them from the beginnig.
    
    This patch causes VM to keep them on memory until last task
    try to reclaim them so shared pages will be reclaimed only if
    all of task has gone swapping out.
    
    This feature doesn't handle non-linear mapping on ramfs because
    it's very time-consuming and doesn't make sure of reclaiming and
    not common.
    
    Signed-off-by: Sangseok Lee <sangseok.lee <at> lge.com>
    Signed-off-by: Minchan Kim <minchan <at> kernel.org>
    ---
    
    Original Patch: http://article.gmane.org/gmane.linux.kernel/1478986
    
    Signed-off-by: revjunkie <rajibrahim@rocketmail.com>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -92,6 +92,13 @@ struct scan_control {
 	 * are scanned.
 	 */
 	nodemask_t	*nodemask;
+	
+	/*
+	 * Reclaim pages from a vma. If the page is shared by other tasks
+	 * it is zapped from a vma without reclaim so it ends up remaining
+	 * on memory until last task zap it.
+	 */
+	struct vm_area_struct *target_vma;
 };
 
 struct mem_cgroup_zone {
@@ -810,7 +817,8 @@ static unsigned long shrink_page_list(struct list_head *page_list,
 		 * processes. Try to unmap it here.
 		 */
 		if (page_mapped(page) && mapping) {
-			switch (try_to_unmap(page, ttu_flags)) {
+			switch (try_to_unmap(page,
+					ttu_flags, sc->target_vma)) {
 			case SWAP_FAIL:
 				goto activate_locked;
 			case SWAP_AGAIN:
@@ -1016,17 +1024,19 @@ unsigned long reclaim_clean_pages_from_list(struct zone *zone,
 }
 
 #ifdef CONFIG_PROCESS_RECLAIM
-unsigned long reclaim_pages_from_list(struct list_head *page_list)
+unsigned long reclaim_pages_from_list(struct list_head *page_list,
+					struct vm_area_struct *vma)
 {
 	struct scan_control sc = {
 		.gfp_mask = GFP_KERNEL,
 		.priority = DEF_PRIORITY,
 		.may_unmap = 1,
 		.may_swap = 1,
+		.target_vma = vma,
 	};
 
-	LIST_HEAD(ret_pages);
 	unsigned long nr_reclaimed;
+	LIST_HEAD(ret_pages);
 	struct page *page;
 	unsigned long dummy1, dummy2;
 
